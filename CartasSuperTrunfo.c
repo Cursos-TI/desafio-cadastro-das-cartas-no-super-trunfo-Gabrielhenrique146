e33e67723902380a3fdb2105cd994a52efa2f719
Gabrielhenrique146 <GABRIELHENRI102030GA@GMAIL.COM>
2025-01-09 16:57:14 +0000

    Adicionado opções do usuario colocar nome, habitantes, pib entre outras opções

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,30 @@
 //Teste larissa
 
 int main() {
+
+    char A01[50]; //Nome das Cidades divididos por codigos, A01, A02, B01, B02
+    float P01; //População das cidades
+    float pibA01; //Pib das Cidades
+    int turA01; //Pontos turisticos da cidade
+    float areaA01; //Area das cidades
+
+    printf("Digite o nome da cidade A01: \n");
+    scanf("%s", &A01);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%f", &P01);
+
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &areaA01);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pibA01);
+
+    printf("Digite o numero de pontos turisticos da cidade: \n");
+    scanf("%d", &turA01);
+
+    printf("Cidade: %s \nPopulação: %f \nÁrea da cidade: %f \nPIB: %f \nPontos turisticos: %d \n", A01, P01, areaA01, pibA01, turA01);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
